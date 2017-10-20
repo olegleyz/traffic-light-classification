a9a009ca85e763c641bed1b4b33447d3d3ad9a40
Oleg Leizerov <olegleyz@gmail.com>
2017-10-20 14:59:54 +0200

    Update train_mobilenet.sh

--- scripts/train_mobilenet.sh
+++ scripts/train_mobilenet.sh
@@ -5,7 +5,7 @@ python src/retrain_sim.py \
 --random_brightness 5 \
 --validation_batch_size -1 \
 --how_many_training_steps 1000 \
---architecture 'mobilenet_1.0_224 \
+--architecture 'mobilenet_1.0_224' \
 --model_di model/mobilenet \
 --test_batch_size=-1 \
 --print_misclassified_test_images=True
