dbe86a1f235915a41b17036a1677176079175c86
Oleg Leizerov <olegleyz@gmail.com>
2017-10-21 15:33:02 +0000

    Add script to move yellow images into red category

---
+++ scripts/yellow2red.py
@@ -0,0 +1,13 @@
+from glob import glob
+import shutil
+import os 
+from tqdm import tqdm
+
+path_udacity = '../data/udacity_data_mix/none/ud_224_yellow*.png'
+path_sim = '../data/sim_binary/none/simyellow*.png'
+ud_imgs = glob(path_udacity)
+sim_imgs = glob(path_sim)
+imgs = ud_imgs + sim_imgs 
+
+for img in tqdm(imgs):
+	shutil(img, os.path.dirname(img)[:-4]+'red')
