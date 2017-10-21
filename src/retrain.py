e4b7b116f74fae1407f2dc3b4cec5e6538ab4e97
Oleg Leizerov <olegleyz@gmail.com>
2017-10-21 22:09:51 +0200

    add tensor name 'keep_prob' for dropout

--- src/retrain.py
+++ src/retrain.py
@@ -722,7 +722,7 @@ def add_final_training_ops(class_count, final_tensor_name, bottleneck_tensor,
      # tf.summary.histogram('pre_activations', logits)
 
   logits = tf.nn.relu(logits)
-  logits = tf.nn.dropout(logits, keep_prob)
+  logits = tf.nn.dropout(logits, keep_prob, name='keep_prob')
   ####
   layer_name = 'final_training_ops'
   with tf.name_scope(layer_name):
