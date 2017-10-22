3235062ce7618a2c9e02ede50693edf905fbe2cc
Oleg Leizerov <olegleyz@gmail.com>
2017-10-22 15:11:42 +0200

    Update train_mobilenet.sh

--- scripts/train_mobilenet.sh
+++ scripts/train_mobilenet.sh
@@ -6,6 +6,7 @@ nohup python src/retrain.py \
 --validation_batch_size -1 \
 --how_many_training_steps 4000 \
 --architecture 'mobilenet_1.0_224' \
---model_di model/mobilenet \
+--keep_probabilities 0.5 \
+--learning_rate 1e-3 \
 --test_batch_size=-1 \
 --print_misclassified_test_images > model/train.log 2>&1 &
