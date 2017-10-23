ec273c972599efaacd6b60cbfcf6f394ad2e6cd9
Ubuntu <ubuntu@ip-172-31-29-21.us-west-1.compute.internal>
2017-10-23 14:07:33 +0000

    update scripts to train entire model

---
+++ scripts/train_mobilenets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#nohup 
+python src/train_model.py \
+--image_dir data/udacity_data_mix \
+--summaries_dir model/summaries_udacity \
+--validation_batch_size -1 \
+--how_many_training_steps 4000 \
+--architecture 'mobilenet_1.0_224' \
+--keep_probabilities 0.5 \
+--learning_rate 1e-2 \
+--test_batch_size=-1 \
+--print_misclassified_test_images
+#> model/train.log 2>&1 &
