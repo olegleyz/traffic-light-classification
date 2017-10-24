fe4c50448fc7c46068ec4797a616ea4a06e6636f
Oleg Leizerov <olegleyz@gmail.com>
2017-10-24 16:03:53 +0200

    add intermediate checkpoints and changed out dir

--- scripts/train_mobilenets.sh
+++ scripts/train_mobilenets.sh
@@ -9,5 +9,8 @@ python scripts/train_model.py \
 --keep_probabilities 0.5 \
 --learning_rate 1e-2 \
 --test_batch_size=-1 \
+--intermediate_output_graphs_dir model/checkpoints\
+--intermediate_store_frequency 100 \
+--output_graph model/final_graph \
 --print_misclassified_test_images
 #> model/train.log 2>&1 &
